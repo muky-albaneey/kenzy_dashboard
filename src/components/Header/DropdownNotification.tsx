5af913a49bc161d56674993b7b8e308b88704c20
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-10 07:04:00 +0100

    product upload

--- src/components/Header/DropdownNotification.tsx
+++ src/components/Header/DropdownNotification.tsx
@@ -58,13 +58,13 @@ const DropdownNotification = () => {
                 >
                   <p className="text-sm">
                     <span className="text-black dark:text-white">
-                      Edit your information in a swipe
+                      Clicked on product to add new stock
                     </span>{' '}
                     Sint occaecat cupidatat non proident, sunt in culpa qui
                     officia deserunt mollit anim.
                   </p>
 
-                  <p className="text-xs">12 May, 2025</p>
+                  <p className="text-xs">2 september, 2024</p>
                 </Link>
               </li>
               <li>
@@ -74,15 +74,15 @@ const DropdownNotification = () => {
                 >
                   <p className="text-sm">
                     <span className="text-black dark:text-white">
-                      It is a long established fact
+                    Clicked on product
                     </span>{' '}
-                    that a reader will be distracted by the readable.
+                    then second option you will see all product to see all uploaded stock
                   </p>
 
                   <p className="text-xs">24 Feb, 2025</p>
                 </Link>
               </li>
-              <li>
+              {/* <li>
                 <Link
                   className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
                   to="#"
@@ -97,8 +97,8 @@ const DropdownNotification = () => {
 
                   <p className="text-xs">04 Jan, 2025</p>
                 </Link>
-              </li>
-              <li>
+              </li> */}
+              {/* <li>
                 <Link
                   className="flex flex-col gap-2.5 border-t border-stroke px-4.5 py-3 hover:bg-gray-2 dark:border-strokedark dark:hover:bg-meta-4"
                   to="#"
@@ -113,7 +113,7 @@ const DropdownNotification = () => {
 
                   <p className="text-xs">01 Dec, 2024</p>
                 </Link>
-              </li>
+              </li> */}
             </ul>
           </div>
         )}
