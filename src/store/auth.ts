8edcd2d9fa29473132b23c371e1fabfbd56c9542
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-16 18:38:04 +0100

    editing zustand

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,3 +1,41 @@
+// import { create } from 'zustand';
+// import { persist, createJSONStorage } from 'zustand/middleware';
+// import { produce } from 'immer';
+
+// type AuthState = {
+//   token: string | null;
+//   user: {
+//     full_name: string;
+//     email: string;
+//   } | null;
+//   isAuthenticated: boolean;
+// //   setAuthData: (token: string, user: { full_name: string; email: string }) => void;
+// setAuthData: (token: string, user: { full_name: string; email: string }) => void;
+
+// };
+
+// export const useAuthStore = create<AuthState>()(
+//   persist(
+//     (set) => ({
+//       token: null,
+//       user: null,
+//       isAuthenticated: false,
+//       setAuthData: (token, user) => {
+//         set(
+//           produce((state) => {
+//             state.token = token;
+//             state.user = user;
+//             state.isAuthenticated = !!token;
+//           })
+//         );
+//       },
+//     }),
+//     {
+//       name: 'auth-storage', // Storage key
+//       storage: createJSONStorage(() => localStorage), // Persist in localStorage (you can use sessionStorage if preferred)
+//     }
+//   )
+// );
 import { create } from 'zustand';
 import { persist, createJSONStorage } from 'zustand/middleware';
 import { produce } from 'immer';
@@ -9,9 +47,7 @@ type AuthState = {
     email: string;
   } | null;
   isAuthenticated: boolean;
-//   setAuthData: (token: string, user: { full_name: string; email: string }) => void;
-setAuthData: (token: string, user: { full_name: string; email: string }) => void;
-
+  setAuthData: (token: string, user: { full_name: string; email: string }) => void;
 };
 
 export const useAuthStore = create<AuthState>()(
