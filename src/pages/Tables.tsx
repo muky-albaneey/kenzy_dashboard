ee7eea65703543f9ec5f6d681b09bfe7fa62a794
Mukarram Abdulkarim <mukyalbani1@gmail.com>
2024-09-18 12:17:50 +0100

    editing product table, fetching all product

--- src/pages/Tables.tsx
+++ src/pages/Tables.tsx
@@ -1,9 +1,16 @@
+import { useNavigate } from 'react-router-dom';
 import Breadcrumb from '../components/Breadcrumbs/Breadcrumb';
 import TableOne from '../components/Tables/TableOne';
+import { useAuthStore } from '../store/auth';
 // import TableThree from '../components/Tables/TableThree';
 // import TableTwo from '../components/Tables/TableTwo';
 
 const Tables = () => {
+
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  const navigate = useNavigate();
+
+  if (!isAuthenticated)   navigate('/auth/signin'); 
   return (
     <>
       <Breadcrumb pageName="Tables" />
